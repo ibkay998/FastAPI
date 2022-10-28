76c69341d7d678a8a002214d4d807f42a1358025
ibkay998 <ibukun46@gmail.com>
2022-10-28 11:57:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://fastapi-production-c585.up.railway.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Msg(BaseModel):
     msg: str
